e4bf4fe20b4b108660e8f071ffaaff53784ecb59
hello-btw <notsure152@proton.me>
2026-07-05 19:15:52 +0100

    Refactor padding alignment for OS, memory, and kernel

--- src/tinyfetch.nim
+++ src/tinyfetch.nim
@@ -22,9 +22,9 @@ let user = getEnv("USER")
 let host = getHostname()
 let kernelData = strutils.splitWhitespace(readFile("/proc/version"))
 let kernelVersion = if kernelData.len > 2: kernelData[2] else: "Unknown"
-let osPadded     = getOSName().runeAlignLeft(25).runeSubstr(0, 24)
-let memPadded    = getMemoryInfo().runeAlignLeft(25).runeSubstr(0, 24)
-let kernelPadded = kernelVersion.runeAlignLeft(25).runeSubstr(0, 24)
+let osPadded     = getOSName().alignLeft(25).runeSubstr(0, 24)
+let memPadded    = getMemoryInfo().alignLeft(25).runeSubstr(0, 24)
+let kernelPadded = kernelVersion.alignLeft(25).runeSubstr(0, 24)
 echo ""
 styledEcho(fgRed, "              <", fgBlue, user, fgRed, ">-", fgYellow, "M", fgRed, "-<", fgBlue, host, fgRed, ">")
 styledEcho(fgMagenta, " .----------------------------------------------.")
