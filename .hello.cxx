71a6a2c317ac0dd4dc902444a65d21f7283a0df8
hello-btw <notsure152@proton.me>
2026-07-10 16:58:01 +0100

    Update .hello.cxx

--- .hello.cxx
+++ .hello.cxx
@@ -1,6 +1,6 @@
 #include <stdio.h>
 // hello-btw
 int main() {
-    printf("\nhello-btw\nwas here\n\ntinyfetch :D\n\n");
+    printf("\nhello-btw\nwas here 󰻀\n\ntinyfetch :D\n\n");
     return 0;
 }
