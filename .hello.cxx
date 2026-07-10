66eedfda3b4f37802d76b5a398b9c277e1219f6e
hello-btw <notsure152@proton.me>
2026-07-10 17:00:44 +0100

    Add sleep and additional message to .hello.cxx

--- .hello.cxx
+++ .hello.cxx
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include <unistd.h>
 // hello-btw
 int main() {
     printf("You found me!\n");
+    sleep(1);
     printf("\nhello-btw was here 󰻀\n\nI hope you enjoy tinyfetch :D\n\n");
     return 0;
 }
